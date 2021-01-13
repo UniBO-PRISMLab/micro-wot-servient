09e919ab3b3a57a8915c2261797b109709e8d92e
Federico Rachelli <f.rachelli@cineca.it>
2021-01-13 09:30:48 +0100

    Add pyserial as dep

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='embeddedWoTServient',
-    url='https://github.com/UniBO-PRISMLab/WoT-microcontroller-servient',
-    author='Daniele Rossi',
+    url='https://github.com/UniBO-PRISMLab/micro-wot-servient',
+    author='Federico Rachelli',
     version='0.1',
     license='MIT',
     description='WoT module for build TDs and executable scripts for embedded systems',
     long_description=open('README.md').read(),
     py_modules=['embeddedWoTServient'],
-    install_requires=['Click','jinja2','pyyaml','jsonschema',],
+    install_requires=['Click','jinja2','pyyaml','jsonschema','pyserial'],
     entry_points='''
         [console_scripts]
         embeddedWoTServient=embeddedWoTServient:cli
